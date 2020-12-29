26572f4ae5ac2f8a684c864c154a57f65ddbd286
0004766 <guodinguo@mininglamp.com>
2020-12-29 10:36:54 +0800

    ADD: jenkins

---
+++ src/main/java/com/example/jekinsdemo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.jekinsdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/demo")
+    public String demo(){
+        return "hello jenkins";
+    }
+}
